3535ee1c7ef77d8e03405f8158ee232499bd1b78
Nattan <nattanferreiralopes@gmail.com>
2025-06-17 17:18:22 -0300

    questões iniciais arraylist

---
+++ q48/Agenda.java
@@ -0,0 +1,11 @@
+package q48;
+
+import java.util.ArrayList;
+
+public class Agenda {
+    private ArrayList<EntradaEmAgenda> entrada;
+
+    public Agenda() {
+        entrada = new ArrayList<>();
+    }
+}
---
+++ q48/EntradaEmAgenda.java
@@ -0,0 +1,24 @@
+package q48;
+
+public class EntradaEmAgenda {
+    private int hora, dia, mes, ano;
+    private String assunto;
+    
+    public EntradaEmAgenda(int hora, int dia, int mes, int ano, String assunto) {
+        this.hora = hora;
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.assunto = assunto;
+    }
+
+    public boolean ehNoDia(int dia, int mes, int ano){
+        return dia == this.dia && mes == this.mes && ano == this.ano;
+    }
+
+    @Override
+    public String toString() {
+        return "hora = " + hora + ", mes = " + mes + ", ano = " + ano + ", assunto = " + assunto;
+    }
+    
+}
