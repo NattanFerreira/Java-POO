a628211098802ed6c8a0cac6752a71bd1169f273
Nattan <nattanferreiralopes@gmail.com>
2025-06-18 20:11:29 -0300

    Corrigindo questoes

--- q65/DemoFuncionario.java
+++ q65/DemoFuncionario.java
@@ -6,11 +6,13 @@ public class DemoFuncionario {
         Assistente f2 = new Assistente("Marcos", 1500, 2025);
         Tecnico f3 = new Tecnico("Joao", 2000, 2020, 100);
         Administrativo f4 = new Administrativo("Santos", 1800, 2021, false, 200);
+        Administrativo f5 = new Administrativo("Jose", 1800, 2021, true, 200);
 
-        System.out.println(f1);
-        System.out.println(f2);
-        System.out.println(f3);
-        System.out.println(f4);
+        System.out.println(f1.toString() + " " + f1.ganhoAnual());
+        System.out.println(f2.toString() + " " + f2.ganhoAnual());
+        System.out.println(f3.toString() + " " + f3.ganhoAnual());
+        System.out.println(f4.toString() + " " + f4.ganhoAnual());
+        System.out.println(f5.toString() + " " + f5.ganhoAnual());
 
     }
 }
